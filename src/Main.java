fa62f22082f95422de8afabb340925e06e49887b
Mateusz Pawlak <mateuszpawlak@interia.pl>
2023-02-26 20:42:57 +0100

    Add src and dzienniczek excel file

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.EventObject;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+
+        MyFrame frame = new MyFrame();
+
+    }
+}
